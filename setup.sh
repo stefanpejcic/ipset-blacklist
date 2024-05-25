a564d38eddababf5b8b15a7cbe81da2269157be3
Stefan Pejcic <stefan@pejcic.rs>
2024-05-25 11:46:42 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
 
+CONFIG_FILE="/etc/openpanel/ufw/blacklists.conf"
+
+set_api_key() {
+    local new_api_key="$1"
+    local temp_file=$(mktemp)
+
+    if [ ! -f "$CONFIG_FILE" ]; then
+        echo "Configuration file $CONFIG_FILE does not exist."
+        return 1
+    fi
+
+    if grep -q "^abuseipdb|" "$CONFIG_FILE"; then
+        sed "s|^abuseipdb|https://api.abuseipdb.com/api/v2/blacklist|.*$|abuseipdb|https://api.abuseipdb.com/api/v2/blacklist|$new_api_key|" "$CONFIG_FILE" > "$temp_file"
+    else
+        echo "abuseipdb|https://api.abuseipdb.com/api/v2/blacklist|$new_api_key" >> "$CONFIG_FILE"
+    fi
+
+    # Replace the original configuration file with the updated one
+    mv "$temp_file" "$CONFIG_FILE"
+    echo "API key for AbuseIPDB is saved in $CONFIG_FILE"
+}
+
 # set configuration
-cp configuration/blacklists.conf  /etc/openpanel/ufw/blacklists.conf
+cp configuration/blacklists.conf /etc/openpanel/ufw/blacklists.conf
 touch /etc/openpanel/ufw/exclude.list
 
 # main file
@@ -15,6 +37,8 @@ systemctl daemon-reload
 systemctl enable ipset-blacklist.timer
 systemctl start ipset-blacklist.timer
 
-
-
-
+# Process flag if present
+if [[ "$1" == "--abuseipdb-key="* ]]; then
+    API_KEY="${1#*=}"
+    set_api_key "$API_KEY"
+fi
