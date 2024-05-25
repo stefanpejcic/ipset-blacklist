1f578cc62d8138b4d0e8c82111a404ab0fcb51a0
Stefan Pejcic <stefan@pejcic.rs>
2024-05-25 11:27:06 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,12 +4,17 @@
 cp configuration/blacklists.conf  /etc/openpanel/ufw/blacklists.conf
 touch /etc/openpanel/ufw/exclude.list
 
+# main file
+cp ipset-blacklist.sh /usr/ipset-blacklist.sh
+chmod +x /usr/ipset-blacklist.sh
 
 # set service
 cp service/ipset-blacklist.service /etc/systemd/system/ipset-blacklist.service
 cp service/ipset-blacklist.timer /etc/systemd/system/ipset-blacklist.timer
+systemctl daemon-reload
+systemctl enable ipset-blacklist.timer
+systemctl start ipset-blacklist.timer
 
-# main file
-cp ipset-blacklist.sh /usr/ipset-blacklist.sh
-chmod +x /usr/ipset-blacklist.sh
+
+
 
